62004b41765d3628a876e1a3c2fc7ca9da6d95d9
Ружилов Артем <ruzhilov.a@promail366.com>
2025-11-18 15:55:14 +0300

    кастомный перехватчик исклчений

--- Web/Program.cs
+++ Web/Program.cs
@@ -2,6 +2,7 @@ using Domain.Interfaces;
 using Infrastructure;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Web.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +16,15 @@ builder.Services.AddScoped<DpContext>(_ => new DpContext(connectionString));
 builder.Services.AddScoped<ITagsRepository, TagsRepository>();
 builder.Services.AddScoped<INewsRepository, NewsRepository>();
 
-
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApiDocument();
 
 builder.Services.AddOpenApi();
+
+// кастомный обработчик ошибок
+builder.Services.AddProblemDetails();
+builder.Services.AddExceptionHandler<ExceptionHandler>(); 
 
 var app = builder.Build();
 
@@ -30,6 +34,9 @@ if (app.Environment.IsDevelopment()) {
 }
 
 app.UseHttpsRedirection();
+
+// кастомный обработчик ошибок
+app.UseExceptionHandler();
 
 app.UseAuthorization();
 
