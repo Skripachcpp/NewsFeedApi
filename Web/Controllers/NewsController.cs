f59b6c92c54554d721f82d40e3eb5862d318e4c9
Ружилов Артем <ruzhilov.a@promail366.com>
2025-11-18 06:13:25 +0300

    swagger

--- Web/Controllers/NewsController.cs
+++ Web/Controllers/NewsController.cs
@@ -4,14 +4,14 @@ namespace Web.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase {
+public class NewsController : ControllerBase {
   private static readonly string[] Summaries = new[] {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
   };
 
-  private readonly ILogger<WeatherForecastController> _logger;
+  private readonly ILogger<NewsController> _logger;
 
-  public WeatherForecastController(ILogger<WeatherForecastController> logger) {
+  public NewsController(ILogger<NewsController> logger) {
     _logger = logger;
   }
 
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,16 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddOpenApiDocument();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
-  app.MapOpenApi();
+  app.UseOpenApi();
+  app.UseSwaggerUi();
 }
 
 app.UseHttpsRedirection();
