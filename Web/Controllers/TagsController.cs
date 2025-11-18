5aca037db65031b90d39a57a0c1af0791a681298
Ружилов Артем <ruzhilov.a@promail366.com>
2025-11-18 10:29:29 +0300

    убрал тестовый метод

--- Web/Controllers/TagsController.cs
+++ Web/Controllers/TagsController.cs
@@ -6,10 +6,5 @@ namespace Web.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class TagsController(ITagsRepository tagsRepository) : ControllerBase {
-  [HttpPost("/v1/test")]
-  public async Task<ActionResult> Test(
-    CancellationToken cancellationToken = default) {
-    await tagsRepository.CreateIfNotExistsAsync(new List<string> {"Спорт", "Популярное"}, cancellationToken);
-    return Ok();
-  }
+  
 }
