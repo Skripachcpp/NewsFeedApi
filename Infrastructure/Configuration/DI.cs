9faf18ec63148808ebec3de971c0fdf2a6d352e1
Ружилов Артем <ruzhilov.a@promail366.com>
2025-11-25 19:02:54 +0300

    fix

--- Infrastructure/Configuration/DI.cs
+++ Infrastructure/Configuration/DI.cs
@@ -5,12 +5,10 @@ namespace Infrastructure.Configuration;
 
 static public class Di
 {
-    static public IServiceCollection AddConfiguration(this IServiceCollection services)
+    static public void AddConfiguration(this IServiceCollection services)
     {
         services.AddScoped<ITagsRepository, TagsRepository>();
         services.AddScoped<INewsRepository, NewsRepository>();
         services.AddScoped<ICacheRepository, CacheRepository>();
-
-        return services;
     }
 }
